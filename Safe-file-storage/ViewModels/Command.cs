4973850b5c616000a0847758ec387676ef0e0770
prochuck <66011374+prochuck@users.noreply.github.com>
2022-12-14 21:06:17 +0300

    Реализована ViewModel для браузера файлов

---
+++ Safe-file-storage/ViewModels/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Safe_file_storage.ViewModels
+{
+    public class Command : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public Command(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute is null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+           _execute(parameter);
+        }
+    }
+}
---
+++ Safe-file-storage/ViewModels/FileBrowserViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Safe_file_storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Safe_file_storage.ViewModels
+{
+    internal class FileBrowserViewModel:INotifyPropertyChanged
+    {
+        FileBrowserModel _fileBrowser;
+
+
+        FileModel _selectedDirectory;
+        public FileModel SelectedDirectory
+        {
+            get { return _selectedDirectory; }
+            set
+            {
+                _selectedDirectory = value;
+                OnProperyChanged(nameof(SelectedDirectory));
+            }
+        }
+
+        public ReadOnlyObservableCollection<FileModel> FilesInDirectory
+        {
+            get
+            {
+                return _fileBrowser.FilesInDirectory;
+            }
+        }
+
+
+        public FileBrowserViewModel(FileBrowserModel fileBrowser)
+        {
+            _fileBrowser = fileBrowser;
+
+            MoveToSelectedDirectory = new Command(e => _fileBrowser.MoveToDirectory(_selectedDirectory), null);
+            MoveToParentDirectory = new Command(e => _fileBrowser.MoveToDirectory(_fileBrowser.CurrentDirectory.ParentDirectoryRecordNo), null);
+
+        }
+
+        public ICommand MoveToSelectedDirectory { get; private set; }
+
+        public ICommand MoveToParentDirectory { get; private set; }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnProperyChanged(string propertyChangedName)
+        {
+            PropertyChanged.Invoke(this,new PropertyChangedEventArgs(propertyChangedName));
+        }
+    }
+}
